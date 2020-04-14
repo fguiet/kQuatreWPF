aff8812398f3d3ed6bef49b109c25dc5b3e66bc7
Frederic Guiet <F.Guiet@brgm.fr>
2020-04-14 20:35:46 +0200

    add missing files

---
+++ src/kQuatre.UI/Command/NavigationContentTemplateSelector.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using Telerik.Windows.Controls;
+
+namespace fr.guiet.kquatre.ui.command
+{
+    public class NavigationContentTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate DesignTemplate { get; set; }
+        public DataTemplate TestTemplate { get; set; }
+        public DataTemplate FireworkTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (item !=  null)
+            {
+                RadNavigationViewItem model = (RadNavigationViewItem)item;
+
+                switch(model.Name)
+                {
+                    case "_nviDesign":
+                        return this.DesignTemplate;
+
+                    case "_nviTest":
+                        return this.TestTemplate;
+
+                    case "_nviFirework":
+                        return this.FireworkTemplate;
+                }
+            }
+               
+            //By default...
+            return this.DesignTemplate;
+
+        }
+    }
+}
