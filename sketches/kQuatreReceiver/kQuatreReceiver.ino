2826326735e862d9b6e49506b911cd9a7ee16c1b
guiet <f.guiet@brgm.fr>
2024-08-28 11:03:52 +0200

    Ajout feu 2024 (fichiers k4 et excel), ajout fichier test boitier 5 et 6

--- sketches/kQuatreReceiver/kQuatreReceiver.ino
+++ sketches/kQuatreReceiver/kQuatreReceiver.ino
@@ -75,7 +75,7 @@ frameObj frame = { "", 0, 0, false };
 /***
  * !!! Modify this !!!
  */
-const String MODULE_ADDRESS = "3";
+const String MODULE_ADDRESS = "6";
 
 void printDebug(String debugMessage) {
  if (DEBUG) {
